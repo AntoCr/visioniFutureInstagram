df7a7b8710ba12ee4a18de17a011791706f48249
AntoCr <add8596.@gmail.com>
2019-01-10 21:20:37 +0100

    adding files

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "downloadAppWording": "Download App",
+    "thumbnail": "Medias/Thumbnails/yt_logo_rgb_dark.png.png",
+    "resumePlaybackWording": "Resume playback?",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "yesWording": "Yes",
+    "title": "Visioni Future",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "noWording": "No",
+    "analyticsKey": "",
+    "redirectToMobileApp": "auto",
+    "launchAppWording": "Then Launch Project",
+    "url": "",
+    "description": "YouTube"
+}
